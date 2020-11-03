26efba4665b511c76488c4daf986b39f51394014
khanj <khanjabed@yahoo.com>
2020-11-02 20:57:49 -0500

    push hahaha

--- pushpullGame/src/test/java/com/usa/pull/pullPull.java
+++ pushpullGame/src/test/java/com/usa/pull/pullPull.java
@@ -5,7 +5,8 @@ public class pullPull {
 	public static void main(String[] args) {
 		System.out.println("pull done");
 		System.out.println("pull today nov 2");
+System.out.println("push from AA");
+System.out.println("hahahaha");
 
-	}
 
-}
+}}
