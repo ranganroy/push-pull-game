5f56dfc16bd556b94503f67fbf3dc777c58eb325
ranga <ranga@DESKTOP-JE0F25C.home>
2020-11-02 20:54:57 -0500

    push nov second

--- pushpullGame/src/test/java/com/usa/pull/pullPull.java
+++ pushpullGame/src/test/java/com/usa/pull/pullPull.java
@@ -5,7 +5,7 @@ public class pullPull {
 	public static void main(String[] args) {
 		System.out.println("pull done");
 		System.out.println("pull today nov 2");
-System.out.println("push from AA");
 
+	}
 
-}}
+}
