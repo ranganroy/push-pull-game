a492f155489279466cdaff8f652a714b6b72b47d
ranga <ranga@DESKTOP-JE0F25C.home>
2020-11-02 21:01:33 -0500

    remove everything

--- pushpullGame/src/test/java/com/usa/pull/pullPull.java
+++ pushpullGame/src/test/java/com/usa/pull/pullPull.java
@@ -3,10 +3,7 @@ package com.usa.pull;
 public class pullPull {
 
 	public static void main(String[] args) {
-		System.out.println("pull done");
-		System.out.println("pull today nov 2");
-System.out.println("push from AA");
-System.out.println("hahahaha");
+		System.out.println("Welcome");
 
 
 }}
